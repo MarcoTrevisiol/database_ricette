78b81d0cfa0af677103f757da5ac1866616fbe84
MarcoTrevisiol <marco.trevisiol@sns.it>
2021-12-30 18:59:03 +0100

    Migliorie al docker-compose

--- docker_preparation.sh
+++ docker_preparation.sh
@@ -2,17 +2,16 @@ read -s -p "Github access token: " github
 echo
 read -s -p "Telegram bot token: " telegram
 echo
-export CATALOGO_PATH=cata
-export BOT_ENV=bot_env
+catalogo_path=catalogo
+www_path="${PWD%/*}/htdocs"
 
-git clone "https://github.com/MarcoTrevisiol/database_ricette_solo_catalogo.git" "${CATALOGO_PATH}"
-git --git-dir="${CATALOGO_PATH}/.git" config credential.helper 'store --file=.git/cre'
-chmod a+w "${CATALOGO_PATH}/catalogo_ricette.json"
-printf "https://MarcoTrevisiol:${github}@github.com\n" >"${CATALOGO_PATH}/.git/cre"
-printf "TOKEN=${telegram}\n" >"${BOT_ENV}"
-printf "CATALOGO_PATH=${PWD}/${CATALOGO_PATH}\nBOT_ENV=${BOT_ENV}\n" >.env
+git clone "https://github.com/MarcoTrevisiol/database_ricette_solo_catalogo.git" "${catalogo_path}"
+git --git-dir="${catalogo_path}/.git" config credential.helper 'store --file=.git/cre'
+chmod a+w "${catalogo_path}/catalogo_ricette.json"
+printf "https://MarcoTrevisiol:${github}@github.com\n" >"${catalogo_path}/.git/cre"
+printf "CATALOGO_PATH=${PWD}/${catalogo_path}\nTOKEN=${telegram}\nWWW_PATH=${www_path}\n" >.env
 for docker_image in docker_image*.tar
 do
-  docker image import "${docker_image}"
+  docker load <"${docker_image}"
 done
 
