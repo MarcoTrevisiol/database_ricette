9329e23656c8ebe1f22dd946e76087cf6b110163
Marco Trevisiol <marco.trevisiol@deimosholding.com>
2025-04-05 12:23:35 +0200

    Implemented levenshtein in python

--- telegram_bot/levenshtein.py
+++ telegram_bot/levenshtein.py
@@ -1,2 +1,24 @@
 def distance(left_str, right_str):
-    return 0 if left_str == right_str else 10
+    memo = {}
+
+    def distance_memoized(a_index, b_index):
+        if a_index < 0:
+            return b_index + 1
+        if b_index < 0:
+            return a_index + 1
+
+        value = memo.get((a_index, b_index), None)
+        if value is None:
+            if left_str[a_index] == right_str[b_index]:
+                value = distance_memoized(a_index - 1, b_index - 1)
+            else:
+                v1 = distance_memoized(a_index - 1, b_index - 1)
+                v2 = distance_memoized(a_index, b_index - 1)
+                v3 = distance_memoized(a_index - 1, b_index)
+                value = 1 + min(v1, v2, v3)
+
+            memo[(a_index, b_index)] = value
+
+        return value
+
+    return distance_memoized(a_index=len(left_str) - 1, b_index=len(right_str) - 1)
